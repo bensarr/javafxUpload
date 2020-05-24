ef098b319b48fa7314f895f8ab0b0e5574d593c8
bensarr <58375575+bensarr@users.noreply.github.com>
2020-05-24 19:48:19 +0000

    Starting Project

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,61 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    @FXML
+    private Label lblError;
+    @FXML
+    private ImageView imgV;
+    private FileChooser fileChooser;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //init Error Label
+        lblError.setVisible(false);
+        lblError.setText("");
 
+        //initialize imageView from Web URL
+        Image image=new Image("https://www.mesinspirationsculinaires.com/wp-content/uploads/2016/07/aid-moubarak-2012.jpeg");
+        imgV.setImage(image);
+
+        //initialize fileChooser extentions
+        fileChooser= new FileChooser();
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("All Images", "*.*"),
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("GIF", "*.gif"),
+                new FileChooser.ExtensionFilter("BMP", "*.bmp"),
+                new FileChooser.ExtensionFilter("PNG", "*.png")
+        );
+    }
+
+    public void change(ActionEvent actionEvent) {
+        fileChooser.setTitle("Change Picture");
+        File file= fileChooser.showOpenDialog(null);
+        try
+        {
+            if(file!=null)
+            {
+                System.out.println(file.getPath());
+                //imgV.setImage(new Image(file.getPath()));
+            }
+        }
+        catch (Exception ex)
+        {
+            lblError.setVisible(true);
+            lblError.setText("Image Incorrect");
+        }
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        root.getStylesheets().add("/sample/style.css");
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
